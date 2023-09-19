f4c34cd176c3e71677bebc2e3a1f7ac7a8e32e3c
Larry Bradley <larry.bradley@gmail.com>
2023-09-19 09:30:59 -0400

    Update sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 #
 # Documentation build configuration file.
@@ -13,39 +12,48 @@
 # the global Astropy configuration which is loaded here before anything
 # else. See astropy.sphinx.conf for which values are set there.
 
-from configparser import ConfigParser
-from datetime import datetime
 import os
 import sys
+from datetime import datetime
+from pathlib import Path
+
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
 
 try:
-    from sphinx_astropy.conf.v1 import *  # noqa
+    from sphinx_astropy.conf.v1 import *  # noqa: F403
 except ImportError:
     print('ERROR: the documentation requires the sphinx-astropy package to '
           'be installed')
     sys.exit(1)
 
-# Get configuration information from setup.cfg
-conf = ConfigParser()
-conf.read([os.path.join(os.path.dirname(__file__), '..', 'setup.cfg')])
-setup_cfg = dict(conf.items('metadata'))
-
+# Get configuration information from pyproject.toml
+with (Path(__file__).parents[1] / 'pyproject.toml').open('rb') as fh:
+    conf = tomllib.load(fh)
+    project_meta = conf['project']
 
 # -- General configuration ----------------------------------------------------
 # By default, highlight as Python 3.
 highlight_language = 'python3'
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.7'
+# needs_sphinx = '3.0'
+
+# To perform a Sphinx version check that needs to be more specific than
+# major.minor, call `check_sphinx_version("x.y.z")` here.
+# check_sphinx_version("1.2.1")
+
+# Extend astropy intersphinx_mapping with packages we use here
+# intersphinx_mapping['skimage'] = ('https://scikit-image.org/docs/stable/', None) noqa: F405
 
 # Exclude astropy intersphinx_mapping for unused packages
-del intersphinx_mapping['h5py']  # noqa
+del intersphinx_mapping['h5py']  # noqa: F405
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns.append('_templates')  # noqa
-# Exclude dev releasing docs
-#exclude_patterns.append('dev/*')  # noqa
+# exclude_patterns.append('_templates')
 
 plot_formats = ['png', 'hires.png', 'pdf', 'svg']
 
@@ -55,14 +63,17 @@ rst_epilog = """
 .. _Astropy: https://www.astropy.org/
 """
 
+# Turn off table of contents entries for functions and classes
+# toc_object_entries = False
 
 # -- Project information ------------------------------------------------------
-project = setup_cfg['name']
-author = setup_cfg['author']
+project = project_meta['name']
+author = project_meta['authors'][0]['name']
 copyright = f'2014-{datetime.utcnow().year}, {author}'
 
-# The version info for the project you're documenting, acts as replacement for  # |version| and |release|, also used in various other places throughout the
-# built documents.
+# The version info for the project you're documenting, acts as
+# replacement for |version| and |release|, also used in various other
+# places throughout the built documents.
 __import__(project)
 package = sys.modules[project]
 
@@ -71,14 +82,14 @@ version = package.__version__.split('-', 1)[0]
 # The full version, including alpha/beta/rc tags.
 release = package.__version__
 
-
 # -- Options for HTML output --------------------------------------------------
+# A NOTE ON HTML THEMES
 # The global astropy configuration uses a custom theme,
-# 'bootstrap-astropy', which is installed along with astropy. A different
-# theme can be used or the options for this theme can be modified by
-# overriding some of the variables set in the global configuration. The
-# variables set in the global configuration are listed below, commented
-# out.
+# 'bootstrap-astropy', which is installed along with astropy. A
+# different theme can be used or the options for this theme can be
+# modified by overriding some of the variables set in the global
+# configuration. The variables set in the global configuration are
+# listed below, commented out.
 
 # Add any paths that contain custom themes here, relative to this
 # directory.
@@ -89,7 +100,7 @@ release = package.__version__
 # to the name of a builtin theme or the name of a custom theme in
 # html_theme_path.
 # html_theme = None
-# Customized theme options
+
 html_theme_options = {
     'logotext1': 'lacosmic',  # white,  semi-bold
     'logotext2': '',  # orange, light
@@ -104,9 +115,9 @@ html_theme_options = {
 # html_logo = ''
 
 # The name of an image file (within the static path) to use as favicon
-# of the docs. This file should be a Windows icon file (.ico) being 16x16
-# or 32x32 pixels large.
-# html_favicon =
+# of the docs. This file should be a Windows icon file (.ico) being
+# 16x16 or 32x32 pixels large.
+# html_favicon = ''
 
 # A "Last built" timestamp is inserted at every page bottom, using the
 # given strftime format. Set to '' to omit this timestamp.
@@ -118,28 +129,22 @@ html_title = f'{project} {release}'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = project + 'doc'
-# Static files to copy after template files
-#html_static_path = ['_static']
-
 
 # -- Options for LaTeX output -------------------------------------------------
 # Grouping the document tree into LaTeX files. List of tuples (source
 # start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [('index', project + '.tex', project + u' Documentation',
+latex_documents = [('index', project + '.tex', project + ' Documentation',
                     author, 'manual')]
-
 
 # -- Options for manual page output -------------------------------------------
 # One entry per manual page. List of tuples (source start file, name,
 # description, authors, manual section).
-man_pages = [('index', project.lower(), project + u' Documentation',
+man_pages = [('index', project.lower(), project + ' Documentation',
               [author], 1)]
 
-
 # -- Resolving issue number to links in changelog -----------------------------
-github_project = setup_cfg['github_project']
+github_project = conf['tool']['build-sphinx']['github_project']
 github_issues_url = f'https://github.com/{github_project}/issues/'
-
 
 # -- Turn on nitpicky mode for sphinx (to warn about references not found) ----
 nitpicky = True
@@ -166,8 +171,6 @@ if os.path.isfile(nitpick_filename):
         dtype, target = line.split(None, 1)
         target = target.strip()
         nitpick_ignore.append((dtype, target))
-
-
 
 # -- Options for linkcheck output ---------------------------------------------
 linkcheck_retry = 5
